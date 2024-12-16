ddf40bfe543929f5ef13675831b8147bf1c7977c
miko <miraskazymurat@gmail.com>
2024-12-16 20:34:12 +0500

    I have added frontend into project

---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="header">
+    <logotype/> 
+    <div class="account">
+      <router-link class="link" to="/authorization">{{user}}</router-link>
+    </div>
+  </div> 
+</template>
+
+<style scoped>
+  .header{
+    padding-top: 5vh ;  
+    display: flex ; 
+    justify-content: space-between ; 
+  }
+  .account{
+    color: white ; 
+    font-weight: 700 ; 
+    font-size: 18px ; 
+  }  
+  .link{
+    text-decoration: none ; 
+    color: white ; 
+  }
+</style>
+
+<script>
+import LogotypeVue from './Logotype.vue'
+export default {
+  data(){
+    return {
+      user: "miko"
+    }
+  } , 
+  components: {
+    'logotype': LogotypeVue 
+  }  
+}
+</script>
---
+++ frontend/src/components/Input.vue
@@ -0,0 +1,25 @@
+<template> 
+  <div class="input">
+    <input v-if="isPrivate" type="password" :placeholder="title" />
+    <input v-else type="text" :placeholder="title" />
+  </div>
+</template>
+
+<style scoped>
+input{
+  border: none ; 
+  background: none ; 
+  outline: none ; 
+  width: 100% ; 
+  padding-bottom: 10px ;  
+  font-size: 16px ; 
+  font-weight: 500 ; 
+  border-bottom: 2px solid black ; 
+}
+</style>
+
+<script>
+export default {
+  props: ["title" , "isPrivate"]
+}
+</script>
